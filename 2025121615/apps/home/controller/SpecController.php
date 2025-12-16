13842047d00195a2dc8be727ead4264cc55f7d9e
jeay <wrjie@msn.cn>
2025-12-16 15:27:24 +0800

    Add update package 2025121615

---
+++ 2025121615/apps/home/controller/SpecController.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\home\controller;
+
+use core\basic\Controller;
+
+class SpecController extends Controller
+{
+    public function index()
+    {
+        return  'SPEC';
+    }
+}
